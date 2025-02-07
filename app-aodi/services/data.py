159441971c4039df14c1931ea40975435e67338b
ADOOSAMA <167531083+ADOOSAMA@users.noreply.github.com>
2025-02-07 14:26:31 +0100

    Aodi- local version
     local version without data lucas_organic_carbon

---
+++ app-aodi/services/data.py
@@ -0,0 +1,196 @@
+import streamlit as st
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import plotly.express as px
+import logging
+from sklearn.datasets import load_iris, load_wine, load_breast_cancer
+import os
+
+base_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'app', 'lucas_organic_carbon')
+
+
+# demo cases
+demo_cases = {
+    'Lucas Organic Carbon (PCA)': {
+         'target': os.path.join(base_path, 'target', 'lucas_organic_carbon_target.csv'),
+        'training': os.path.join(base_path, 'training_test', 'compressed_data.csv')
+    },
+    'Lucas Organic Carbon (AE)': {
+        'target': os.path.join(base_path, 'target', 'lucas_organic_carbon_target.csv'),
+        'training': os.path.join(base_path, 'training_test', 'autoencoder_predictions.csv')
+    },
+
+    'Iris Dataset': {
+        'sklearn_dataset': load_iris
+    },
+    'Wine Dataset': {
+        'sklearn_dataset': load_wine
+    },
+    'Breast Cancer Dataset': {
+        'sklearn_dataset': load_breast_cancer
+    },
+    'Custom Data': {
+        'custom': True
+    }
+}
+
+preset_target = demo_cases['Lucas Organic Carbon (PCA)']['target']
+preset_training = demo_cases['Lucas Organic Carbon (PCA)']['training']
+
+# load data
+@st.cache_data(show_spinner=False)
+def load_data(target_data=None, training_data=None, selected_demo_case='Lucas Organic Carbon (PCA)'):
+    """
+    Load the data for training.
+    """
+    
+    # load data from sklearn
+    if 'sklearn_dataset' in demo_cases[selected_demo_case]:
+        logging.info(f"Loading {selected_demo_case} data from sklearn.")
+        data = demo_cases[selected_demo_case]['sklearn_dataset']()
+        df = pd.DataFrame(data.data, columns=data.feature_names)
+        df['target'] = data.target
+        df_combined = df
+        df_training = df.drop(columns=['target'])
+        df_target = df[['target']]
+        logging.info(f"Loaded {selected_demo_case} data from sklearn.")
+        logging.info(f"Training data shape: {df_training.shape}")
+        logging.info(f"Target data shape: {df_target.shape}")
+        logging.info(f"Combined data shape: {df_combined.shape}")
+
+    # load custom data
+    elif 'custom' in demo_cases[selected_demo_case]:
+        logging.info("Loading custom data.")
+        if st.session_state['custom_training'] and st.session_state['custom_target']:
+            df_target = target_data
+            df_training = training_data
+            logging.info("Loaded custom target and training data from uploaded files.")
+        else:
+            logging.error("Full custom data not provided. Please upload both target and training data.")
+            return None, None, None
+
+        df_combined = pd.merge(df_training, df_target, left_index=True, right_index=True)
+
+    # load data from presets or given paths
+    else:
+        df_target = pd.read_csv(demo_cases[selected_demo_case]['target'])
+        logging.info("Loaded target data from preset.")
+
+        df_training = pd.read_csv(demo_cases[selected_demo_case]['training'])
+        logging.info("Loaded training data from preset.")
+        
+        df_combined = pd.merge(df_training, df_target, left_index=True, right_index=True)
+        logging.info("Merged target and training data successfully.")
+
+    return df_training, df_target, df_combined
+
+# prepare data
+@st.cache_data(show_spinner=False)
+def prepare_data(df_combined, data_percentage):
+    """
+    Prepare the data for training.
+    """
+
+    # sample the data
+    sample_size = int(len(df_combined) * (data_percentage / 100))
+    df_sampled = df_combined.sample(n=sample_size, random_state=42)
+    predictors = df_sampled.columns[:-1]
+
+    # prepare the data
+    target = df_sampled.columns[-1]
+    X = df_sampled[predictors]
+    y = df_sampled[target]
+
+    # encode categorical target
+    label_encoder = LabelEncoder()
+    y = label_encoder.fit_transform(y)
+
+    # split the data with error handling
+    try:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        logging.info("Data prepared successfully.")
+    except ValueError:
+        logging.error(f"Data preparation failed. Percentage of data used: {data_percentage}.")
+        # st.warning("Data preparation failed. Please adjust percentage of data used.")
+        return None, None, None, None, None
+
+    return X_train, X_test, y_train, y_test, label_encoder
+
+def plot_target_distribution(df_combined):
+    """
+    Plot target distribution.
+    """
+
+    target_column = df_combined.columns[-1]
+
+    # create histogram
+    fig = px.histogram(df_combined, x=target_column, color=target_column, title='', 
+                       labels={target_column: 'Target Class'}, 
+                       template='plotly_dark',
+                       category_orders={target_column: df_combined[target_column].value_counts().index})
+    fig.update_layout(margin=dict(l=20, r=20, t=20, b=20), yaxis_title='Count')
+
+    # update hover template
+    fig.update_traces(hovertemplate='Class: %{x}<br>Count: %{y}<extra></extra>')
+
+    # display the figure
+    with st.expander("**Target Distribution**", expanded=True):
+        st.plotly_chart(fig, key='target_distribution')
+        logging.info("Target distribution displayed successfully.")
+
+def plot_feature_profiles(df_combined):
+    """
+    Plot feature profiles.
+    """
+
+    target_column = df_combined.columns[-1]
+
+    with st.expander("**Feature Profiles**", expanded=True):
+        # sample feature profiles
+        num_samples = st.slider('Number of Samples', min_value=1, max_value=10, value=3, key='num_samples_feature')
+
+        # select a few feature profiles to display
+        sample_feature_profiles = df_combined.sample(num_samples).drop(columns=[target_column])
+
+        # plot feature profiles
+        fig_feature = px.line(title='')
+        for idx, row in sample_feature_profiles.iterrows():
+            fig_feature.add_scatter(x=row.index.astype(str), y=row.values, mode='lines', name=f'Sample {idx}', hovertemplate='Feature: %{x}<br>Value: %{y}<extra></extra>')
+        fig_feature.update_layout(xaxis_title='Feature', yaxis_title='Value', template='plotly_dark', margin=dict(l=20, r=20, t=20, b=20))
+        st.plotly_chart(fig_feature, key='feature_profiles')
+
+        logging.info("Feature profiles displayed successfully.")
+
+def plot_feature_distribution(df_combined):
+    """
+    Plot feature distribution.
+    """
+
+    target_column = df_combined.columns[-1]
+
+    # select a few features to display (first, 1/4, 1/2, 3/4, last)
+    feature_columns = df_combined.columns[:-1]
+    selected_features = [
+        feature_columns[0],
+        feature_columns[len(feature_columns) // 4],
+        feature_columns[len(feature_columns) // 2],
+        feature_columns[3 * len(feature_columns) // 4],
+        feature_columns[-1]
+    ]
+
+    # melt the DataFrame for plotting (wide to long format)
+    df_melted = df_combined.melt(id_vars=target_column, value_vars=selected_features, var_name='Feature', value_name='Value')
+
+    # create box plot
+    fig_boxplot = px.box(df_melted, x='Feature', y='Value', color=target_column, title='', template='plotly_dark',
+                         labels={target_column: 'Target Class'})
+    fig_boxplot.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+
+    # update hover template
+    fig_boxplot.update_traces(hovertemplate='Feature: %{x}<br>Value: %{y}<extra></extra>')
+
+    # display the figure
+    with st.expander("**Feature Distribution**", expanded=True):
+        st.plotly_chart(fig_boxplot, key='feature_distribution')
+        logging.info("Feature distribution displayed successfully.")
---
+++ app-aodi/services/error_analysis.py
@@ -0,0 +1,211 @@
+import logging
+import pandas as pd
+import streamlit as st
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+import plotly.graph_objects as go
+
+from config import SELECTION_COLOR, BLUE
+
+def _display_overall_metrics(accuracy, precision, recall, f1):
+    """
+    Display the overall metrics.
+    """
+
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("Accuracy", f"{accuracy:.2f}")
+    col2.metric("Precision", f"{precision:.2f}")
+    col3.metric("Recall", f"{recall:.2f}")
+    col4.metric("F1 Score", f"{f1:.2f}")
+    logging.info("Overall metrics displayed successfully.")
+
+def _display_class_counts(y_test, y_pred, unique_labels, selected_class):
+    """
+    Display the class counts as a histogram.
+    """
+
+    # count instances of each class in the test set
+    actual_class_counts = pd.Series(y_test).value_counts().reindex(range(len(unique_labels)), fill_value=0)
+    predicted_class_counts = pd.Series(y_pred).value_counts().reindex(range(len(unique_labels)), fill_value=0)
+
+    # sort by count
+    sorted_counts = actual_class_counts.sort_values(ascending=False)
+    sorted_labels = [unique_labels[i] for i in sorted_counts.index]
+
+    # create a bar chart
+    fig = go.Figure(data=[
+        go.Bar(
+            name='Actual',
+            x=sorted_labels, 
+            y=sorted_counts.values, 
+            marker_color='black',
+            marker_line_width=1,
+            marker_line_color=[SELECTION_COLOR if label == selected_class else 'black' for label in sorted_labels],
+            hovertemplate='Class: %{x}<br>Count: %{y}<extra></extra>'
+        ),
+        go.Bar(
+            name='Predicted',
+            x=sorted_labels, 
+            y=predicted_class_counts[sorted_counts.index].values, 
+            marker_color='grey',
+            marker_line_color=[SELECTION_COLOR if label == selected_class else 'grey' for label in sorted_labels],
+            marker_line_width=1,
+            hovertemplate='Class: %{x}<br>Count: %{y}<extra></extra>'
+        )
+    ])
+
+    # update layout
+    fig.update_layout(
+        xaxis_title="Class",
+        yaxis_title="Count",
+        margin=dict(l=20, r=20, t=20, b=20),
+        height=300, # reduce height
+        barmode='group', # group bars
+        xaxis=dict(
+            tickmode='array',
+            tickvals=list(range(len(sorted_labels))),
+            ticktext=[f'<span style="color:{SELECTION_COLOR};">{label}</span>' if label == selected_class else label for label in sorted_labels]
+        )
+    )
+
+    # display the figure
+    st.plotly_chart(fig)
+    logging.info("Class counts displayed as histogram successfully.")
+
+def _display_class_metrics(y_test, y_pred, selected_class, selected_class_index):
+    """
+    Display the metrics for the selected class.
+    """
+
+    class_y_test = (y_test == selected_class_index).astype(int)
+    class_y_pred = (y_pred == selected_class_index).astype(int)
+    class_accuracy = accuracy_score(class_y_test, class_y_pred)
+    class_precision = precision_score(class_y_test, class_y_pred)
+    class_recall = recall_score(class_y_test, class_y_pred)
+    class_f1 = f1_score(class_y_test, class_y_pred)
+
+    # compute confusion matrix
+    cm_error = False
+    try:
+        tn, fp, fn, tp = confusion_matrix(class_y_test, class_y_pred).ravel()
+    except ValueError as e:
+        cm_error = True
+        tn, fp, fn, tp = 0, 0, 0, 0
+        logging.error(f"Error in computing confusion matrix: {e}")
+
+    # display metrics
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("Accuracy", f"{class_accuracy:.2f}")
+    col2.metric("Precision", f"{class_precision:.2f}")
+    col3.metric("Recall", f"{class_recall:.2f}")
+    col4.metric("F1 Score", f"{class_f1:.2f}")
+
+    # display confusion matrix metrics
+    col1, col2, col3, col4 = st.columns(4)
+    col1.metric("True Positives", tp)
+    col2.metric("True Negatives", tn)
+    col3.metric("False Positives", fp)
+    col4.metric("False Negatives", fn)
+
+    if cm_error:
+        st.error("Confusion matrix did not return enough values. Metrics may not be accurate.")
+
+    logging.info(f"Metrics for class {selected_class} displayed successfully.")    
+
+def _display_confusion_matrix(cm, unique_labels, selected_class_index):
+    """
+    Display the confusion matrix.
+    """
+    
+    fig = go.Figure(data=go.Heatmap(
+        z=cm,
+        x=[f'{label}' for label in unique_labels],
+        y=[f'{label}' for label in unique_labels],
+        colorscale=[[0, 'white'], [1, BLUE]],
+        showscale=True,
+        hovertemplate='Predicted: %{x}<br>Actual: %{y}<br>Value: %{z}<extra></extra>'
+    ))
+
+    # confusion matrix: improve layout 
+    fig.update_layout(
+        xaxis_title='Predicted Label',
+        yaxis_title='Actual Label',
+        xaxis=dict(
+            tickmode='array', 
+            tickvals=list(range(len(unique_labels))), 
+            ticktext=[f'<span style="color:{SELECTION_COLOR};">{label}</span>' if i == selected_class_index else label for i, label in enumerate(unique_labels)]
+        ),
+        yaxis=dict(
+            tickmode='array', 
+            tickvals=list(range(len(unique_labels))), 
+            ticktext=[f'<span style="color:{SELECTION_COLOR};">{label}</span>' if i == selected_class_index else label for i, label in enumerate(unique_labels)]
+        ),
+        margin=dict(l=20, r=20, t=20, b=20)
+    )
+
+    # add a rectangle shape to create a border effect
+    fig.add_shape(
+        type="rect",
+        x0=-0.5, x1=len(unique_labels) - 0.5,
+        y0=-0.5, y1=len(unique_labels) - 0.5,
+        line=dict(color="black", width=1),
+        fillcolor='rgba(0,0,0,0)'
+    )
+
+     # add grid lines
+    for i in range(len(unique_labels) + 1):
+        fig.add_shape(
+            type='line',
+            x0=i - 0.5,
+            x1=i - 0.5,
+            y0=-0.5,
+            y1=len(unique_labels) - 0.5,
+            line=dict(color='white', width=1)
+    )
+    
+    for i in range(len(unique_labels) + 1):
+        fig.add_shape(
+            type='line',
+            x0=-0.5,
+            x1=len(unique_labels) - 0.5,
+            y0=i - 0.5,
+            y1=i - 0.5,
+            line=dict(color='white', width=1)
+    )
+
+    # add rectangles to highlight the selected class (row and column)
+    fig.add_shape(
+        type="rect",
+        x0=-0.5, x1=len(unique_labels) - 0.5,
+        y0=selected_class_index - 0.5, y1=selected_class_index + 0.5,
+        line=dict(color=SELECTION_COLOR, width=1),
+    )
+    fig.add_shape(
+        type="rect",
+        x0=selected_class_index - 0.5, x1=selected_class_index + 0.5,
+        y0=-0.5, y1=len(unique_labels) - 0.5,
+        line=dict(color=SELECTION_COLOR, width=1),
+    )
+    
+    # display the figure
+    st.plotly_chart(fig)
+    logging.info("Confusion matrix displayed successfully.")
+
+# show results
+def visualize_error_analysis(y_test, y_pred, unique_labels, selected_class, selected_class_index, accuracy, precision, recall, f1, cm):
+    """
+    Display the error analysis results.
+    """
+
+    # display overall metrics
+    with st.expander("**Overall Metrics**", expanded=True):
+        _display_overall_metrics(accuracy, precision, recall, f1)
+
+    # display metrics for the selected class
+    with st.expander(f"**Metrics for Class: {selected_class}**", expanded=True):
+       _display_class_metrics(y_test, y_pred, selected_class, selected_class_index)
+
+    with st.expander("**Class Counts**", expanded=True):
+        _display_class_counts(y_test, y_pred, unique_labels, selected_class)
+
+    with st.expander("**Confusion Matrix**", expanded=True):
+        _display_confusion_matrix(cm, unique_labels, selected_class_index)
